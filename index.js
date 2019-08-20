ebffbc4fa7205f533dda40eda177cc95560a0301
Julio Rivero <jeriveromartinez@gmail.com>
2019-08-20 17:13:38 -0500

    [FIX] Bugs.

--- index.js
+++ index.js
@@ -2,64 +2,41 @@
 
 /***
 
-- inspired by https://gist.github.com/numist/f34cb150e337a8b948d9
-- we can also run this as both a module and shell script
 - this currently only works on *NIX machines (no windows support)
 
 ***/
 
-var fs     = require('fs');
-var crypto = require('crypto');
-var glob   = require("glob");
+const fs = require('fs');
+const crypto = require('crypto');
+const glob = require("glob");
 
-var trim = function(string){
-  string = string.toString();
-  string = string.replace(/\r/g, '');
-  string = string.replace(/\n/g, '');
-  return string;
+const trimFile = function (string) {
+    string = string.toString();
+    string = string.replace(/\r/g, '');
+    string = string.replace(/\n/g, '');
+    return string;
 };
 
-var timezone = function(){
-  var string;
-
-  if(process.env.TZ){ 
-    return process.env.TZ; 
-  }
-
-  else if( fs.existsSync('/etc/timezone') ){
-    string = fs.readFileSync('/etc/timezone'); 
-    return trim(string);
-  }
-
-  else if( fs.lstatSync('/etc/localtime').isSymbolicLink() ){
-    string = fs.readlinkSync('/etc/localtime');
-    string = string.replace('/usr/share/zoneinfo/', '');
-    return trim(string);
-  }
-
-  else{
-    var md5sum = crypto.createHash('md5');
-    md5sum.update( fs.readFileSync('/etc/localtime') );
-    var sourceMd5 = md5sum.digest('hex');
-    var files = glob.sync('/usr/share/zoneinfo/**/*');
-    for(var i in files){
-      var file = files[i];
-      if( fs.statSync(file).isFile() ){
-        var localMd5Sum = crypto.createHash('md5');
-        localMd5Sum.update( fs.readFileSync(file) );
-        var localMd5 = localMd5Sum.digest('hex');
-        if( localMd5 === sourceMd5 ){
-          string = file.replace('/usr/share/zoneinfo/', '');
-          return trim(string);
+const timezone = function () {
+    if (process.env.TZ) return process.env.TZ;
+    else if (fs.existsSync('/etc/timezone')) return trimFile(fs.readFileSync('/etc/timezone'));
+    else if (fs.lstatSync('/etc/localtime').isSymbolicLink()) {
+        const string = fs.readlinkSync('/etc/localtime').replace('/usr/share/zoneinfo/', '');
+        return trimFile(string);
+    } else {
+        const md5sum = crypto.createHash('md5').update(fs.readFileSync('/etc/localtime'));
+        const sourceMd5 = md5sum.digest('hex');
+        const files = glob.sync('/usr/share/zoneinfo/**/*');
+        for (const file of files) {
+            if (fs.statSync(file).isFile()) {
+                const localMd5Sum = crypto.createHash('md5').update(fs.readFileSync(file));
+                const localMd5 = localMd5Sum.digest('hex');
+                if (localMd5 === sourceMd5) return trimFile(file.replace('/usr/share/zoneinfo/', ''));
+            }
         }
-      }
     }
-  }
 
-  throw new Error('cannot determine this system\'s timezone');
+    throw new Error('Unavailable show system timezone.');
 };
-
-///
 
 module.exports = timezone;
-if(!module.parent){ console.log( timezone() ); }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,3 @@
 var system_timezone = require('../index.js');
 
-describe('system tiemzone', function(){
-  it('should not crash', function(){
-    var timezone = system_timezone();
-    console.log('  timezone -> ' + timezone);
-  }); 
-});
+describe('System Timezone', () => it('Not crash', () => console.log(`TimeZone:: ${system_timezone()}`)));
